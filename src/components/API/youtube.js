527f984e7783e90450dac92057ffaa1d229d424b
adim <adim.wbdev@gmail.com>
2022-09-06 15:36:29 +0300

    Commit

---
+++ src/components/API/youtube.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBAUYpvghjjY_v1QqmDA7dKW9necGuMWSY'
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params:{ 
+        part: 'snippet',
+        type:'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
+
+
